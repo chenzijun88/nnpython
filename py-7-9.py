ffef0af497731fcc6d41039ed9d72031d22d283a
chenzijun88 <41565019+chenzijun88@users.noreply.github.com>
2020-11-25 16:06:17 +0800

    Add files via upload

---
+++ py-7-9.py
@@ -0,0 +1,23 @@
+# ==
+# !=
+# >
+# >=
+# <
+# <=
+
+# print(1 == 2)
+# print(1 != 2)
+# print(1 > 2)
+# print(1 >= 2)
+# print(1 < 2)
+# print(1 <= 2)
+
+# print(1 == '1')
+
+# and or not
+
+print(1 < 2 and 1!= 2)
+print(1 > 2 and 1 != 2)
+print(1 > 2 or 1 != 2)
+print(1 > 2 or 1 == 2)
+print(not 1 < 2)
